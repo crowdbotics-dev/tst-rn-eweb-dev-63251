8ea7c965d031e94a67835692b566cbc40fefeaf6
vinay sharma <vinay.sharma@crowdbotics.com>
2023-02-13 18:13:36 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "tst-rn-eweb-dev-63251",
   url: "https://tst-rn-eweb-dev-63251.botics.co",
   api: "https://tst-rn-eweb-dev-63251.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
